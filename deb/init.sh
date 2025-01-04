70f4e171354e49963307cc99685cbcdee0618bdb
Pouria Rezaei <Pouria.rz@outlook.com>
2025-01-04 13:05:32 +0330

    Improvements + Fixes
    - Added LICENSE
    - Applied small fixes
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>

--- deb/init.sh
+++ deb/init.sh
@@ -4,30 +4,24 @@
 
 # Copyright 2025 Pouria Rezaei <Pouria.rz@outlook.com>
 # All rights reserved.
-#
-# Redistribution and use of this script, with or without modification, is
-# permitted provided that the following conditions are met:
-#
-# 1. Redistributions of this script must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
-#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
-#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License at <http://www.gnu.org/licenses/> for
+# more details.
 
 # General Variables
 MIRROR="http://mirror.ox.ac.uk"
-Port="22"
+PORT="22"
 
 #Check user privilege
-if [[ $(id -u) != "0" ]]; then
+if [ $(id -u) != "0" ]; then
 	echo "Script needs to run under superuser."
 	exit 1
 fi
@@ -43,13 +37,13 @@ apt install dosfstools debootstrap arch-install-scripts -y
 set -e
 
 echo 'y' | mkfs.fat -F 32 /dev/sda1
-#echo 'y' | mkswap -L /dev/sda2
 echo 'y' | mkfs.ext4 /dev/sda3
+mkswap /dev/sda2
 
 mount /dev/sda3 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
-  
+
 # wget $(python3 -c "import requests, re; \
 #         url = 'http://archive.debian.org/debian/pool/main/d/debootstrap/'; \
 #         response = requests.get(url); \
@@ -64,25 +58,27 @@ cat > "/mnt/etc/ssh/sshd_config.d/50-Debian.conf" <<SSH
 Port $PORT
 PermitRootLogin yes
 PasswordAuthentication yes
+
 SSH
 
 debootstrap --arch amd64 \
-    --include=sudo,locales,vim,wget,ca-certificates,curl,systemd-timesyncd,neofetch,zstd,parted,cron,dosfstools,git,openssh-server,build-essential,python3-venv,python3-pip,linux-image-amd64,linux-headers-amd64,grub-efi-amd64,open-vm-tools \
+    --include=sudo,dbus,locales,vim,wget,ca-certificates,curl,systemd-timesyncd,neofetch,zstd,parted,cron,dosfstools,git,openssh-server,build-essential,python3-venv,python3-pip,grub-efi-amd64,open-vm-tools \
         bookworm /mnt $MIRROR/debian
 
-swapon /dev/sda2
 mount --rbind /dev /mnt/dev
 mount --rbind /sys /mnt/sys
 mount --rbind /proc /mnt/proc
 mount --rbind /tmp /mnt/tmp
+swapon /dev/sda2
 
 genfstab -U /mnt > /mnt/etc/fstab
 cp -rf /etc/network/interfaces /mnt/etc/network/
 echo -e "\nneofetch\n" >> /mnt/etc/profile
 
-cat >> /mnt/etc/default/locale <<LCL
+cat > /mnt/etc/default/locale <<LCL
 LANG=en_US.UTF-8
 LC_ALL=en_US.UTF-8
+
 LCL
 
 cat > /mnt/etc/apt/sources.list <<MGM
@@ -98,12 +94,13 @@ deb http://security.debian.org/debian-security/ bookworm-security contrib main n
 
 MGM
 
-ch_exec "apt update && \
-    locale-gen && \
-    apt dist-upgrade --autoremove -y"
+ch_exec "locale-gen; \
+    apt update && \
+    apt dist-upgrade --autoremove -y && \
+    apt install -y linux-image-amd64 linux-headers-amd64 firmware-linux"
 
-ch_exec "grub-install --force --removable && \
-    sed -i '/etc/default/grub' -e 's/=5/=0/' && \
+ch_exec "sed -i '/etc/default/grub' -e 's/=5/=0/'; \
+    grub-install --force --removable && \
     grub-mkconfig -o /boot/grub/grub.cfg"
 
-ch_exec "passwd"
+ch_exec "passwd; bash"
